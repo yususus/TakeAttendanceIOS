fcec4f680fc3fb44fd3774a8501441ce9de85fc1
Yusuf Aydın <yusayd3825@gmail.com>
2024-05-04 21:20:58 +0300

    Initial Commit

---
+++ TakeAttendanceIOS/TakeAttendanceIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeAttendanceIOSApp.swift
+//  TakeAttendanceIOS
+//
+//  Created by yusuf on 4.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TakeAttendanceIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
